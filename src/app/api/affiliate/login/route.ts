6f27017129d6e8a9721ed3944ec15a548a7104da
Daxia Vlora Dmello <116895227+vloradmello@users.noreply.github.com>
2025-04-25 15:27:46 +0530

    Update route.ts

--- src/app/api/affiliate/login/route.ts
+++ src/app/api/affiliate/login/route.ts
@@ -1,6 +1,6 @@
 import { NextRequest, NextResponse } from 'next/server';
 import pool from '@/lib/db';
-const bcrypt = require('bcrypt');
+import bcrypt from 'bcrypt';
 
 export async function POST(req: NextRequest) {
   try {

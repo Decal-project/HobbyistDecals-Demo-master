bae3e8c151f1c749e59695091994e79780a79d49
Daxia Vlora Dmello <116895227+vloradmello@users.noreply.github.com>
2025-05-07 17:09:46 +0530

    Update statbox.tsx

--- src/app/(main)/hobbyist-decals/_components/statbox.tsx
+++ src/app/(main)/hobbyist-decals/_components/statbox.tsx
@@ -13,7 +13,7 @@ const StatBox = ({ title, end, inView }: { title: string, end: number, inView: b
   return (
     <div className="bg-white rounded-lg p-6 shadow text-center">
       <h2 className="text-4xl font-bold text-[#16689A]">
-        {inView ? <CountUp end={displayValue} duration={2} suffix={suffix} /> : `0${suffix}`}
+        {inView ? <CountUp end={displayValue} duration={6} suffix={suffix} /> : `0${suffix}`}
       </h2>
       <p className="mt-2 text-sm font-semibold text-gray-800">{title}</p>
     </div>

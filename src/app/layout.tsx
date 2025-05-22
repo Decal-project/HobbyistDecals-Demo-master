d4784f62318c804763674e22e85a4d663e97b4a5
Daxia Vlora Dmello <116895227+vloradmello@users.noreply.github.com>
2025-05-22 09:37:50 +0530

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,8 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Providers } from "./(main)/_components/providers";
 import ClientLayout from "./(main)/_components/client-layout";
+import ChatbotIcon from "./(main)/_components/chatbotIcon";
+import ScrollToTopLink from "@/components/global/ScrollToTopLink"; 
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -28,7 +30,9 @@ export default function RootLayout({
     <html lang="en" className={`${geistSans.variable} ${geistMono.variable}`}>
       <body className="min-h-fit w-full flex flex-col items-center justify-center antialiased">
         <Providers>
+          <ScrollToTopLink /> {/* 👈 Scroll to top on route change */}
           <ClientLayout>{children}</ClientLayout>
+          <ChatbotIcon />
         </Providers>
       </body>
     </html>

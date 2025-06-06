a9b4f0ada0ce1b00820009eaf6572006a51310de
Daxia Vlora Dmello <116895227+vloradmello@users.noreply.github.com>
2025-06-06 16:25:35 +0530

    Update get-in-touch.tsx

--- src/components/global/get-in-touch.tsx
+++ src/components/global/get-in-touch.tsx
@@ -24,7 +24,7 @@ export default function GetinTouch() {
         </a>
 
         <Link href="/contact-us">
-            <button className="bg-[#16689A] text-white text-lg px-5 py-3 rounded flex items-center space-x-2 hover:bg-black transition">
+            <button className="bg-[#BEE2F3] text-black text-lg px-5 py-3 rounded flex items-center space-x-2 hover:bg-black transition">
                 <span>Contact Us</span>
             </button>
         </Link>
